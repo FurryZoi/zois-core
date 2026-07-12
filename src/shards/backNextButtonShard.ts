b58ae0bfc0e823b668b00fc05ad303bd1f39cd88
Zoi <volkof06@mail.ru>
2026-07-12 17:04:16 +0300

    Improve backNextButton shard

--- src/shards/backNextButtonShard.ts
+++ src/shards/backNextButtonShard.ts
@@ -1,36 +1,74 @@
-// import { Anchor, autosetFontSize, setFontFamily, setFontSize } from "../ui";
+import { addDynamicClass, autosetFontSize, DynamicClassStyles } from "../ui";
 import { Shard, ShardContext } from "./shard";
 
 export interface BackNextButtonShardContext extends ShardContext<"backButton" | "nextButton" | "text"> {
     items: [string, any?][]
     currentIndex: number
-    isBold?: boolean
     onChange?: (value: any) => void
     isDisabled?: (value: any) => boolean
 }
 
 export class BackNextButtonShard extends Shard<BackNextButtonShardContext> {
+    protected get dynamicClassContainer(): DynamicClassStyles {
+        return {
+            base: {
+                display: "flex",
+                columnGap: "2vw",
+                justifyContent: "center",
+                alignItems: "center",
+                background: "var(--tmd- element, white)",
+                color: "var(--tmd-text, black)",
+                border: "2px solid var(--tmd-accent, black)",
+                borderRadius: "4px"
+            }
+        }
+    }
+
+    protected get dynamicClassButton(): DynamicClassStyles {
+        return {
+            base: {
+                cursor: "pointer",
+                background: "var(--tmd-element, white)",
+                color: "var(--tmd-text, black)",
+                border: "2px solid var(--tmd-accent, rgb(34, 34, 34))",
+                borderRadius: "6px",
+            },
+            hover: {
+                background: "var(--tmd-element-hover, #ebf7fe)",
+                borderColor: "var(--tmd-accent-hover, #7dd3fc)",
+                color: "var(--tmd-accent-hover, #015a8c)"
+            },
+            disabled: {
+                background: "var(--tmd-element-disabled, #ffa590)",
+                pointerEvents: "none"
+            }
+        };
+    }
+
     override generateBody(): Record<keyof NonNullable<BackNextButtonShardContext["modules"]>, HTMLElement | SVGElement> {
-        let { items, currentIndex, isBold, onChange, isDisabled } = CommonCloneDeep(this.context);
+        const { onChange, isDisabled } = this.context;
         const div = document.createElement("div");
-        div.classList.add("zcBackNextButton");
-        // setFontFamily(div, MOD_DATA.fontFamily);
+        addDynamicClass(div, this.dynamicClassContainer);
+        let currentIndex = this.context.currentIndex;
+        let items = this.context.items;
 
         const updateClasses = () => {
             if (
                 currentIndex === 0 ||
                 (
-                    typeof isDisabled === "function" && isDisabled(items[currentIndex - 1][1])
+                    typeof isDisabled === "function" &&
+                    isDisabled(items[currentIndex - 1][1])
                 )
-            ) backBtn.classList.add("zcBackNextButton-btnDisabled");
-            else backBtn.classList.remove("zcBackNextButton-btnDisabled");
+            ) backBtn.disabled = true;
+            else backBtn.disabled = false;
             if (
                 currentIndex === items.length - 1 ||
                 (
-                    typeof isDisabled === "function" && isDisabled(items[currentIndex + 1][1])
+                    typeof isDisabled === "function" &&
+                    isDisabled(items[currentIndex + 1][1])
                 )
-            ) nextBtn.classList.add("zcBackNextButton-btnDisabled");
-            else nextBtn.classList.remove("zcBackNextButton-btnDisabled");
+            ) nextBtn.disabled = true;
+            else nextBtn.disabled = false;;
         }
 
         const backBtn = document.createElement("button");
@@ -38,7 +76,7 @@ export class BackNextButtonShard extends Shard<BackNextButtonShardContext> {
                 position: absolute; left: 1vw; font-size: 3.5vw; aspect-ratio: 1/1;
                 height: 140%; background-image: url("Icons/Prev.png"); background-size: 100%;
                 `;
-        backBtn.classList.add("zcButton");
+        addDynamicClass(backBtn, this.dynamicClassButton);
         backBtn.addEventListener("click", () => {
             if (currentIndex === 0) return backBtn.classList.add("zcDisabled");
             if (typeof isDisabled === "function" && isDisabled(items[currentIndex - 1][1])) return backBtn.classList.add("zcDisabled");
@@ -53,7 +91,7 @@ export class BackNextButtonShard extends Shard<BackNextButtonShardContext> {
                 position: absolute; right: 1vw; font-size: 3.5vw; aspect-ratio: 1/1;
                 height: 140%; background-image: url("Icons/Next.png"); background-size: 100%;
                 `;
-        nextBtn.classList.add("zcButton");
+        addDynamicClass(nextBtn, this.dynamicClassButton);
         nextBtn.addEventListener("click", () => {
             if (currentIndex === items.length - 1) return nextBtn.classList.add("zcDisabled");
             if (typeof isDisabled === "function" && isDisabled(items[currentIndex + 1][1])) return nextBtn.classList.add("zcDisabled");
@@ -65,8 +103,7 @@ export class BackNextButtonShard extends Shard<BackNextButtonShardContext> {
 
         updateClasses();
 
-        const text = document.createElement("p");
-        if (isBold) text.style.fontWeight = "bold";
+        const text = document.createElement("b");
         text.textContent = items[currentIndex][0];
 
         div.append(backBtn, text, nextBtn);
@@ -77,5 +114,10 @@ export class BackNextButtonShard extends Shard<BackNextButtonShardContext> {
             nextButton: nextBtn,
             text
         };
+    }
+
+    override update(): void {
+        super.update();
+        autosetFontSize(this.body!.base as HTMLElement);
     }
 }
